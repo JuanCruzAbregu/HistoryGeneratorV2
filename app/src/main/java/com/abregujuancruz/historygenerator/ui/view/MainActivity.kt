af1e32e22f10cec9bb863b4dff761234dae075d5
Juan Cruz Abregu <abregu.juancruz@gmail.com>
2022-07-07 19:04:36 -0300

    Some themes and shimmer

--- app/src/main/java/com/abregujuancruz/historygenerator/ui/view/MainActivity.kt
+++ app/src/main/java/com/abregujuancruz/historygenerator/ui/view/MainActivity.kt
@@ -1,6 +1,7 @@
 package com.abregujuancruz.historygenerator.ui.view
 
 import android.os.Bundle
+import android.view.View
 import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
 import androidx.recyclerview.widget.LinearLayoutManager
@@ -13,28 +14,33 @@ import dagger.hilt.android.AndroidEntryPoint
 @AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
     
-    private val historyViewModel : HistoryViewModel by viewModels()
+    private val historyViewModel: HistoryViewModel by viewModels()
     private lateinit var binding: ActivityMainBinding
-    private lateinit var historyList : ArrayList<History>
+    private lateinit var historyList: ArrayList<History>
     
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
         
-        binding.btnGenerate.setOnClickListener{
+        binding.btnGenerate.setOnClickListener {
             historyViewModel.getListOfHistories()
         }
         
-        historyViewModel.historyData.observe(this){
+        historyViewModel.visibility.observe(this) {
+            binding.shimmerLayout.visibility = if (it) View.VISIBLE else View.GONE
+            binding.rvHistory.visibility = if(it) View.GONE else View.VISIBLE
+        }
+        
+        historyViewModel.historyData.observe(this) {
             if (it != null) {
-                historyList  = it
+                historyList = it
             }
             initRecyclerView(historyList)
         }
     }
     
-    private fun initRecyclerView(historyList : ArrayList<History>) {
+    private fun initRecyclerView(historyList: ArrayList<History>) {
         binding.rvHistory.adapter = HistoryAdapter(historyList)
         binding.rvHistory.layoutManager = LinearLayoutManager(this)
     }
--- app/src/main/java/com/abregujuancruz/historygenerator/ui/viewmodel/HistoryViewModel.kt
+++ app/src/main/java/com/abregujuancruz/historygenerator/ui/viewmodel/HistoryViewModel.kt
@@ -1,5 +1,8 @@
 package com.abregujuancruz.historygenerator.ui.viewmodel
 
+import android.os.Handler
+import android.os.Looper
+import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
@@ -20,23 +23,24 @@ class HistoryViewModel @Inject constructor(
     private val _historyData = MutableLiveData<HistoryResponse?>()
     val historyData: MutableLiveData<HistoryResponse?> get() = _historyData
     
+    private val _visibility = MutableLiveData<Boolean>()
+    val visibility: LiveData<Boolean> get() = _visibility
+    
     fun getListOfHistories() {
         viewModelScope.launch {
+            _visibility.value = true
             loadHistoriesUseCase().enqueue(object : Callback<HistoryResponse> {
                 override fun onResponse(
                     call: Call<HistoryResponse>,
                     response: Response<HistoryResponse>
                 ) {
                     _historyData.value = response.body()
+                    _visibility.value = false
                 }
-                
                 override fun onFailure(call: Call<HistoryResponse>, t: Throwable) {
                     _historyData.value = null
                 }
-                
             })
         }
     }
-    
-    
 }
