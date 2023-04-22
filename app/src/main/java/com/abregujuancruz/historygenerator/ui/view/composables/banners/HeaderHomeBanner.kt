b107a09458795b96173e4c67e40bd4608f597be9
Juan Cruz ABREGU <juan.abregu@naranjax.com>
2023-04-22 17:02:36 -0300

    Ejemplo de git

--- app/src/main/java/com/abregujuancruz/historygenerator/ui/view/composables/banners/HeaderHomeBanner.kt
+++ app/src/main/java/com/abregujuancruz/historygenerator/ui/view/composables/banners/HeaderHomeBanner.kt
@@ -39,7 +39,7 @@ fun HeaderHomeBanner(
                     .height(40.dp),
             ) {
                 Text(
-                    text = "Generar",
+                    text = "Hola",
                     fontSize = 14.sp,
                     fontWeight = FontWeight.Bold
                 )
